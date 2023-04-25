3593dd67c28fc7f8f053cb3009982fa56c487151
EPICGAMING191 <112524668+EPICGAMING191@users.noreply.github.com>
2023-04-25 11:13:12 -0400

    Add files via upload

---
+++ template.py
@@ -0,0 +1,19 @@
+import discord
+
+intents=discord.Intents.default()
+intents.message_content= True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(INSERT TOKEN HERE)
